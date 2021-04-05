7bc816fef4bce61ec970c4f87e80494b600231b7
lordmaltazor <malte.svensen@hotmail.com>
2021-04-05 11:17:44 +0200

    added output area and run button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import downloadAsFile from 'download-as-file';
+import EditorJS from '@editorjs/editorjs';
 
 function App() {
     const [text, setText] = useState('');
     const [filename, setFilename] = useState('');
+
+    const [output, setOutput] = useState(''); // The output of the users code
+    const [error, setError] = useState(false); // Whether or not the was an error in the user's code
+
+    const updateCode = (e) => {
+        setText(e.target.value);
+    }
 
     const downloadFile = () => {
         downloadAsFile({
@@ -13,16 +21,34 @@ function App() {
         })
     }
 
+    const runCode = () => {
+        try {
+            setOutput(eval(text));
+            setError(false);
+        }
+        catch (err) {
+            //console.log(err);
+
+            setOutput(`${err.name}: ${err.message}`);
+            setError(true);
+        }
+    }
+
     return (
         <div className="app">
             <header>
                 <p className="file-name">Filename: </p>
                 <input type="text" onChange={(e) => setFilename(e.target.value)} />
 
-                <button className="download-button" onClick={downloadFile}>Download File</button>
+                <button className="header-button" onClick={downloadFile}>Download File</button>
+                <button className="header-button" onClick={runCode}>Run</button>
             </header>
 
-            <textarea onChange={(e) => setText(e.target.value)} placeholder="Type here!" spellCheck="false"></textarea>
+            <section className="main-section">
+                <textarea onChange={updateCode} placeholder="Type your Javascript code here!" spellCheck="false"></textarea>
+
+                <div className="output" style={{ color: error ? "red" : 'white' }}>{output}</div>
+            </section>
         </div>
     );
 }
